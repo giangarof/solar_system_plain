50fe048488b99cc9661a9ef4be9428ec61fd5cbd
Gianmarco G <giangarof98@gmail.com>
2024-07-23 14:48:02 -0400

    js toggle and data

--- index.js
+++ index.js
@@ -1,7 +1,26 @@
 
-function toggle(){
-    // const info = document.getElementsByClassName('text');
-    // info.classList.toggle('visible');
-    alert('')
+function toggle(planet){
+    // Find the text element associated with the clicked planet
+    const textElement = planet.nextElementSibling;
+
+    // Check if the text element is already visible
+    const isVisible = textElement.classList.contains('show');
+
+    // Hide all text elements
+    const allTextElements = document.querySelectorAll('.text');
+    allTextElements.forEach(text => text.classList.remove('show'));
+
+    // If the text element was not visible, show it
+    if (!isVisible) {
+        textElement.classList.add('show');
+    }
 }
+
+document.addEventListener('click', function(event) {
+    const target = event.target;
+    if (!target.closest('.this')) {
+        const allTextElements = document.querySelectorAll('.text');
+        allTextElements.forEach(text => text.classList.remove('show'));
+    }
+});
 
